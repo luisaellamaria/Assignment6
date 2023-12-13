d7642e3d2b52149e05932f0567a4b6d1921c6802
luisaella <luisaella.mueller@student.unisg.ch>
2023-12-13 23:21:41 +0100

    I have added app.js and improved the ui

--- Task 3/app.js
+++ Task 3/app.js
@@ -1,64 +1,62 @@
 const express = require("express");
-const cors = require('cors'); 
+const cors = require('cors');
 const app = express();
 const http = require('http');
 const path = require('path');
 const httpServer = http.Server(app);
 const { Server } = require("socket.io");
 const io = new Server(httpServer);
-
 const QueryEngine = require('@comunica/query-sparql').QueryEngine;
-
 const engine = new QueryEngine();
-
 const port = 5000;
 
-app.use(cors())
-
-app.use(express.json())
-
-app.use(express.static('public'))
-
-
+app.use(cors());
+app.use(express.json());
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.statusCode = 200;
     res.setHeader("Content-Type", "text/html");
     res.sendFile(path.join(__dirname, 'index.html'));
-  });
+});
 
-  app.get('/movies', (req, res) => {
-      let input_url = req.query.url
-      console.log("input url: ", input_url)
-      res.statusCode = 200
-      res.setHeader("Content-Type", "text/html");
-      res.sendFile(path.join(__dirname, 'index.html'));
-      handle_query(input_url)
-  });
+app.get('/movies', (req, res) => {
+    let input_url = req.query.url;
+    console.log("input url: ", input_url);
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/html");
+    res.sendFile(path.join(__dirname, 'index.html'));
+    handle_query(input_url);
+});
 
-  async function handle_query(input_url){
-    movie_urls=[] //List of movies in the pod
-    engine.queryBindings(``, { //TODO: complete SPARQL query
-    sources: [input_url],
-  }).then(function (bindingsStream) {
-    bindingsStream.on('data', function (data) {
-      movie_urls.push(data.get('v').value)
+async function handle_query(input_url) {
+    engine.queryBindings(`
+        PREFIX schema: <https://schema.org/>
+        SELECT ?name ?image ?description ?datePublished
+        WHERE {
+            ?movie a schema:Movie.
+            ?movie schema:name ?name.
+            ?movie schema:image ?image.
+            ?movie schema:description ?description.
+            ?movie schema:datePublished ?datePublished.
+        }
+    `, {
+        sources: [input_url],
+    }).then(function (bindingsStream) {
+        bindingsStream.on('data', function (data) {
+            let obj = {
+                "name": data.get('name').value,
+                "image": data.get('image').value,
+                "description": data.get('description').value,
+                "datePublished": data.get('datePublished').value
+            };
+            io.emit('update', {'message': obj});
+        });
+        // Handle end of stream and errors...
+    }).catch(error => {
+        console.error(error);
+        // Handle error
     });
-    engine.queryBindings(``, { //TODO: complete SPARQL query
-    sources: movie_urls,
-  }).then(function (bindingsStream) {
-    bindingsStream.on('data', function (data) {
-      obj = {
-        "name": data.get('name').value,
-        "image": data.get('image').value
-      };
-      io.emit('update', {'message': obj})
-    });
-  });
-  });
-  }
-  
-  
-  
-  httpServer.listen(port), 
-      () => console.log("Server is running... on "+port);
+}
+
+httpServer.listen(port, () => console.log("Server is running on port " + port));
