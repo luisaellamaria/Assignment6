0a8b7077459055872f25ba6a6745c3e025e9db93
luisa ella mueller <127049546+luisaellamaria@users.noreply.github.com>
2023-12-20 13:33:52 +0100

    Updated app.js

--- Task 3/app.js
+++ Task 3/app.js
@@ -8,7 +8,9 @@ const { Server } = require("socket.io");
 const io = new Server(httpServer);
 const QueryEngine = require('@comunica/query-sparql').QueryEngine;
 const engine = new QueryEngine();
-const port = 5000;
+
+const port = 5001;
+let storedMovies = []; // Global variable to store movies
 
 app.use(cors());
 app.use(express.json());
@@ -22,41 +24,69 @@ app.get('/', (req, res) => {
 
 app.get('/movies', (req, res) => {
     let input_url = req.query.url;
+    let filterLetter = req.query.letter;
     console.log("input url: ", input_url);
     res.statusCode = 200;
     res.setHeader("Content-Type", "text/html");
     res.sendFile(path.join(__dirname, 'index.html'));
-    handle_query(input_url);
+    handle_query(input_url, filterLetter);
 });
 
-async function handle_query(input_url) {
-    engine.queryBindings(`
-        PREFIX schema: <https://schema.org/>
-        SELECT ?name ?image ?description ?datePublished
-        WHERE {
-            ?movie a schema:Movie.
-            ?movie schema:name ?name.
-            ?movie schema:image ?image.
-            ?movie schema:description ?description.
-            ?movie schema:datePublished ?datePublished.
+async function handle_query(input_url, filterLetter) {
+    let movie_urls = [];
+    storedMovies = []; // Reset stored movies
+
+    // First query to fetch the URLs of all items in the container
+    await engine.queryBindings(`
+        PREFIX ldp: <http://www.w3.org/ns/ldp#>
+        SELECT ?movie WHERE {
+            ?container ldp:contains ?movie .
         }
-    `, {
-        sources: [input_url],
-    }).then(function (bindingsStream) {
+    `, { sources: [input_url] }).then(async function (bindingsStream) {
+        // Process each binding and push movie URLs to the array
         bindingsStream.on('data', function (data) {
-            let obj = {
-                "name": data.get('name').value,
-                "image": data.get('image').value,
-                "description": data.get('description').value,
-                "datePublished": data.get('datePublished').value
-            };
-            io.emit('update', {'message': obj});
+            movie_urls.push(data.get('movie').value);
         });
-        // Handle end of stream and errors...
-    }).catch(error => {
-        console.error(error);
-        // Handle error
+
+        await new Promise(resolve => bindingsStream.on('end', resolve));
+
+        // Second query to fetch the name and image of each movie
+        await engine.queryBindings(`
+            PREFIX schema: <https://schema.org/>
+            SELECT ?name ?image WHERE {
+                ?movie schema:name ?name;
+                       schema:image ?image .
+            }
+        `, { sources: movie_urls }).then(function (bindingsStream) {
+            console.log("Movie URLs: ", movie_urls);
+
+            bindingsStream.on('data', function (data) {
+                let obj = {
+                    "name": data.get('name').value,
+                    "image": data.get('image').value
+                };
+                storedMovies.push(obj); // Store each movie
+            });
+
+            bindingsStream.on('end', function() {
+                if (filterLetter) {
+                    filterAndEmitMovies(filterLetter);
+                } else {
+                    storedMovies.forEach(movie => io.emit('update', {'message': movie}));
+                }
+            });
+        });
     });
 }
 
-httpServer.listen(port, () => console.log("Server is running on port " + port));
+function filterAndEmitMovies(letter) {
+    const lowercaseLetter = letter.toLowerCase();
+    const filteredMovies = storedMovies.filter(movie => movie.name.toLowerCase().startsWith(lowercaseLetter));
+    filteredMovies.forEach(movie => io.emit('update', {'message': movie}));
+}
+
+
+httpServer.listen(port, () => {
+    console.log("Server is running on port " + port);
+});
+
